1f505142441ed20afde58f0f462d6a4068cc6dd3
Makoto Otsuka <mametra2580@gmail.com>
2018-02-07 11:19:13 +0900

    add List

--- vue/tutorial.vue
+++ vue/tutorial.vue
@@ -1,8 +1,21 @@
 window.onload = function() {
+  // Model
   var demo = new Vue({
     el: '#demo',
     data: {
       message: 'Hello Vue'
+    }
+  });
+
+  // List
+  new Vue({
+    el: '#app',
+    data: {
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue.js' },
+        { text: 'Build Something Awesome' }
+      ]
     }
   });
 };
