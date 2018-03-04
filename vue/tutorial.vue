e7cbaaf73a82c9bbb43652c73959ff09c2d594f1
Makoto Otsuka <mametra2580@gmail.com>
2018-03-04 18:15:55 +0900

    add login

--- vue/tutorial.vue
+++ vue/tutorial.vue
@@ -175,6 +175,35 @@ window.onload = function() {
     }
   });
 
+  // ログイン
+  var auth = {
+    login: function(id, pass) {
+      window.alert("login id: " + id + "\n" + "password: " + pass);
+    }
+  };
+
+  var loginTemplate = '<div><input type="text" placeholder="ログインID" v-model="userid"><input type="password" placeholder="パスワード" v-model="password"><button v-on:click="login">ログイン</button></div>';
+
+  Vue.component('user-login', {
+    template: loginTemplate,
+    data: function() {
+      return {
+        userid: '',
+        password: ''
+      }
+    },
+    methods: {
+      login: function() {
+        auth.login(this.userid, this.password);
+      }
+    }
+  });
+
+  new Vue({
+    el: '#login-example'
+  });
+
+
 };
 
 
