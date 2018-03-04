966930919033f964e9f2d1e94efc0ce180154e9b
Makoto Otsuka <mametra2580@gmail.com>
2018-03-04 16:38:31 +0900

    add props

--- vue/tutorial.vue
+++ vue/tutorial.vue
@@ -125,6 +125,23 @@ window.onload = function() {
     }
   });
 
+  // props
+  Vue.component('fruits-list', {
+    props: ['fruitsItem'],
+    template: '<li>{{fruitsItem.name}}</li>'
+  });
+
+  new Vue({
+    el: '#fruits-component',
+    data: {
+      fruitsItems: [
+        {name: '梨'},
+        {name: 'イチゴ'}
+      ]
+    }
+  });
+
+
 };
 
 
