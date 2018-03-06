7b8bfc2040495059404b01df769f1840a9393173
Makoto Otsuka <mametra2580@gmail.com>
2018-03-06 11:31:31 +0900

    add children

--- vue/spa.vue
+++ vue/spa.vue
@@ -1,3 +1,30 @@
+// ユーザー詳細ページのコンポーネント定義
+var User = {
+  template:
+    '<div class="user">' +
+      '<h2>ユーザーIDは {{ $route.params.userId }} です。</h2>' +
+      '<router-link :to="\'/user/\' + $route.params.userId + \'/profile\'">ユーザーのプロフィールページを見る</router-link>' +
+      '<router-link :to="\'/user/\' + $route.params.userId + \'/posts\'">ユーザーの投稿ページを見る</router-link>' +
+      '<router-view></router-view>' +
+    '</div>'
+}
+
+// ユーザー詳細ページ内で部分的に表示されるユーザーのプロフィールページ
+var UserProfile = {
+  template:
+    '<div class="user-profile">' +
+      '<h3>こちらはユーザー {{ $route.params.userId }} のプロフィールページです。</h3>' +
+    '</div>'
+}
+
+// ユーザー詳細ページ内で部分的に表示されるユーザーの投稿ページ
+var UserPosts = {
+  template:
+    '<div class="user-posts">' +
+      '<h3>こちらはユーザー {{ $route.params.userId }} の投稿ページです。</h3>' +
+    '</div>'
+}
+
 // ルートオプションを渡してルーターインスタンスを生成します
 var router = new VueRouter({
   // 各ルートにコンポーネントをマッピングします
@@ -15,7 +42,22 @@ var router = new VueRouter({
       component: {
         template: '<div>ユーザー一覧ページです。</div>'
       }
-    }
+    },
+    {
+      path: '/user/:userid',
+      name: 'user',
+      component: User,
+      children: [
+        {
+          path: 'profile',
+          component: UserProfile
+        },
+        {
+          path: 'posts',
+          component: UserPosts
+        }
+      ]
+    },
   ]
 })
 
