aad8e7a6c4796004a80fdff55dda47b8072d30f7
Makoto Otsuka <mametra2580@gmail.com>
2018-03-06 10:40:18 +0900

    add vue-router basic

---
+++ vue/spa.vue
@@ -0,0 +1,29 @@
+window.onload = function() {
+  // ルートオプションを渡してルーターインスタンスを生成します
+  var router = new VueRouter({
+    // 各ルートにコンポーネントをマッピングします
+    // コンポーネントはVue.extend() によって作られたコンポーネントコンストラクタでも
+    // コンポーネントオプションのオブジェクトでも構いません
+    routes: [
+      {
+        path: '/top',
+        component: {
+          template: '<div>トップページです。</div>'
+        }
+      },
+      {
+        path: '/users',
+        component: {
+          template: '<div>ユーザー一覧ページです。</div>'
+        }
+      }
+    ]
+  });
+
+  // ルーターのインスタンスをrootとなるVueインスタンスに渡します
+  var app = new Vue({
+    router: router
+  }).$mount('#app');
+
+  window.router = router;
+};
