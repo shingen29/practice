8be4864cd656ae34e28e2230187246ee2128c11e
Makoto Otsuka <mametra2580@gmail.com>
2018-07-28 11:08:37 +0900

    Add 2.1.6

---
+++ perfect_php/2.1.6.php
@@ -0,0 +1,17 @@
+<?php
+
+$a = 1;
+$b = "foo bar";
+$c = array(1,2,3);
+
+
+echo $a."\n";
+echo $b."\n";
+echo $c."\n";
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+
+var_dump($a == $a);
+var_dump($a == $b);
